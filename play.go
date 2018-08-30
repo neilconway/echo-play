b4be44ac4be1940bcd8b47c6cd0e34b6743ac7fa
Neil Conway <neil@determined.ai>
2018-08-30 15:18:12 -0700

    WIP

--- play.go
+++ play.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/labstack/echo"
+import (
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo"
+)
+
+var (
+	upgrader = websocket.Upgrader{}
+)
 
 type Master struct {
 }
@@ -21,6 +28,32 @@ func (h *Handler) getAgentSlot(c echo.Context) error {
 	return nil
 }
 
+func (h *Handler) agentWebSocket(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	// Read startup message.
+	_, msg, err := ws.ReadMessage()
+	if err != nil {
+		c.Logger().Error(err)
+	}
+
+	return nil
+}
+
+func (h *Handler) trialWebSocket(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	return nil
+}
+
 func main() {
 	m := &Master{}
 	h := &Handler{
@@ -31,6 +64,9 @@ func main() {
 	e.GET("/agents", h.getAgents)
 	e.GET("/agents/:agent_id", h.getAgent)
 	e.GET("/agents/:agent_id/slots/:slot_id", h.getAgentSlot)
+
+	e.GET("/ws/agent", h.agentWebSocket)
+	e.GET("/ws/trial", h.trialWebSocket)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
